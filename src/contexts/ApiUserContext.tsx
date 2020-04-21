ea0ba09bf79202061f6f6b5e07175f4da73afbdb
Andrew Davis <atd285@gmail.com>
2020-04-20 21:06:38 -0400

    some better optimization to apicontext provider 🙌

--- src/contexts/ApiUserContext.tsx
+++ src/contexts/ApiUserContext.tsx
@@ -1,29 +1,56 @@
 import React from 'react';
 import client from '../utils/client';
-import { useHistory } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 const APIClientContext = React.createContext({});
 const { Provider, Consumer } = APIClientContext;
 export const ApiProviderContext = Provider;
 export const ApiConsumerContext = Consumer;
 
+const history = createBrowserHistory();
+
 function SpotifyClientProvider(props: any) {
-  let history = useHistory();
-  const [client, setClient] = React.useState(() => {
-    if (props.client) {
-      return props.client;
+  // let history = useHistory();
+  const [user, setUser] = React.useState(() => {
+    if (props.user) {
+      return props.user;
     }
     const token = window.localStorage.getItem('spotify-token');
     if (token) {
-      setClient(token);
+      setUser(token);
     }
   });
+  const [error, setError] = React.useState(null);
   React.useEffect(() => {
     if (!client) {
       history.push('/');
     }
+
+    // if the user navigates away from the page, zap them back home
+    const unlisten = history.listen(() => {
+      if (!client) {
+        history.push('/');
+      }
+    });
+
     return () => {
-      console.log('unmounting!');
+      unlisten();
     };
   });
+
+  return user ? (
+    <SpotifyClientProvider value={user}>{props.children}</SpotifyClientProvider>
+  ) : (
+    <div>
+      {error ? (
+        <pre>{JSON.stringify(error, null, 2)}</pre>
+      ) : (
+        <button>
+          Login with spotify right this very second for an awesome experience!
+        </button>
+      )}
+    </div>
+  );
 }
+
+export { SpotifyClientProvider as Provider };
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import * as SpotifyContext from './contexts/ApiUserContext';
 import ThemeContext from './contexts/ThemeContext';
 import GlobalStyles from './shared/global-styles';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { LoadingMessagePage } from './shared/pattern';
 const User = React.lazy(() => import('./screens/user'));
-const App = React.lazy(() => import('./app'));
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeContext>
-      <GlobalStyles />
-      <React.Suspense
-        fallback={
-          <LoadingMessagePage>application is loading!</LoadingMessagePage>
-        }
-      >
-        <Router>
-          <Route exact path="/" component={App} />
-          <Route path="/user/:username" component={User} />
-        </Router>
-      </React.Suspense>
-    </ThemeContext>
+    <SpotifyContext.Provider>
+      <ThemeContext>
+        <GlobalStyles />
+        <React.Suspense
+          fallback={
+            <LoadingMessagePage>application is loading!</LoadingMessagePage>
+          }
+        >
+          <Router>
+            <Route path="/user/:username" component={User} />
+          </Router>
+        </React.Suspense>
+      </ThemeContext>
+    </SpotifyContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
