ee48caad21abd1bd10dfcb3d366e933bbd874624
Andrew Davis <atd285@gmail.com>
2020-04-21 15:36:04 -0400

    handshake with spotify api

--- src/contexts/ApiUserContext.tsx
+++ src/contexts/ApiUserContext.tsx
@@ -1,13 +1,15 @@
+/**@jsx jsx*/
 import React from 'react';
 import client from '../utils/client';
 import { createBrowserHistory } from 'history';
-
+import { jsx } from '@emotion/core';
+import { PrimaryLoginButton } from '../shared/pattern';
 const APIClientContext = React.createContext({});
 const { Provider, Consumer } = APIClientContext;
 export const ApiProviderContext = Provider;
 export const ApiConsumerContext = Consumer;
-
 const history = createBrowserHistory();
+const BASE_URL = 'https://accounts.spotify.com/authorize';
 
 function SpotifyClientProvider(props: any) {
   // let history = useHistory();
@@ -20,7 +22,7 @@ function SpotifyClientProvider(props: any) {
       setUser(token);
     }
   });
-  const [error, setError] = React.useState(null);
+  const [error, setError] = React.useState<null | string>(null);
   React.useEffect(() => {
     if (!client) {
       history.push('/');
@@ -38,16 +40,31 @@ function SpotifyClientProvider(props: any) {
     };
   });
 
+  const login = async () => {
+    // here we will make the call to spotify
+    const qs = `?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=token`;
+    const data = await client(`${BASE_URL}${qs}`).catch((error) => {
+      setError('Oh no, there was an error!');
+    });
+    console.log(data);
+  };
+
   return user ? (
     <SpotifyClientProvider value={user}>{props.children}</SpotifyClientProvider>
   ) : (
-    <div>
+    <div
+      css={{
+        display: 'flex',
+        marginTop: 300,
+        justifyContent: 'center',
+      }}
+    >
       {error ? (
         <pre>{JSON.stringify(error, null, 2)}</pre>
       ) : (
-        <button>
-          Login with spotify right this very second for an awesome experience!
-        </button>
+        <PrimaryLoginButton onClick={login}>
+          Login with Spotify
+        </PrimaryLoginButton>
       )}
     </div>
   );
