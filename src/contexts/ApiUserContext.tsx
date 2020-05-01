c036baa0127df1160956f4ebf8d125e5bae1a663
Andrew Davis <atd285@gmail.com>
2020-04-30 20:40:38 -0400

    brainstorming new ideas for login logic

--- src/contexts/ApiUserContext.tsx
+++ src/contexts/ApiUserContext.tsx
@@ -18,6 +18,9 @@ const BASE_URL = 'https://accounts.spotify.com/authorize';
 function SpotifyClientProvider(props: any) {
   const [client, setClient] = React.useState<any>(() => {
     const token = localStorage.getItem('spotify-token');
+    if (props.client) {
+      return props.client;
+    }
     if (!token) {
       // if there's an existing token
       return getClient(token);
@@ -25,23 +28,28 @@ function SpotifyClientProvider(props: any) {
   });
   const [error, setError] = React.useState<null | string>(null);
 
-  const token = window.localStorage.getItem('spotify-token');
   React.useEffect(() => {
-    if (token) {
-      // redirect the user somewhere
-      console.log('logging in!');
-    } else {
-      const params: any = getHashParams();
-      getClient(params.access_token);
+    console.log(client);
+    if (!client) {
+      history.push('/');
     }
-  }, [token]);
+    // listen for changes to the current location
+    const unsubscribe = history.listen(() => {
+      if (!client) {
+        history.push('/');
+      }
+    });
+    return function cleanup() {
+      unsubscribe();
+    };
+  });
   // exchanges client information for an access token for web api access.
   const handShake = () => {
-    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-    const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-    const queryString = `?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=${scopes}`;
-    const url = `${BASE_URL}${queryString}`;
-    window.location.assign(url);
+    // const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+    // const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+    // const queryString = `?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=${scopes}`;
+    // const url = `${BASE_URL}${queryString}`;
+    // window.location.assign(url);
   };
 
   // s/o to stackoverflow for this awesome function to retrieve a hash value!
@@ -59,16 +67,20 @@ function SpotifyClientProvider(props: any) {
 
   // function to log user into the spotify API
   async function getClient(token) {
-    window.localStorage.setItem('spotify-token', token);
-    const request = await fetch(`https://api.spotify.com/v1/me`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const response = await request.json();
-    setClient(response);
-    history.push(`/user/${response.id}`);
+    const a = window.localStorage.setItem('spotify-token', '1234');
+    // const request = await fetch(`https://api.spotify.com/v1/me`, {
+    //   method: 'GET',
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
+    // const response = await request.json();
+    return token;
+    // history.push(`/user/${response.id}`);
+  }
+
+  async function login() {
+    setClient(getClient({ name: 'andrew' }));
   }
 
   return client ? (
@@ -85,7 +97,7 @@ function SpotifyClientProvider(props: any) {
         {error ? (
           <pre>{JSON.stringify(error, null, 2)}</pre>
         ) : (
-          <PrimaryLoginButton onClick={handShake}>
+          <PrimaryLoginButton onClick={login}>
             Login with Spotify
           </PrimaryLoginButton>
         )}
